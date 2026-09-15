da445f38d4271e413f0c47d162836b56f312f6d1
Wellison M. Dias <wellisonmdias@gmail.com>
2026-09-15 11:44:58 -0300

    feat: implements the home page view

--- src/Views/Home.php
+++ src/Views/Home.php
@@ -1,11 +1,63 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
     <head>
+        <title>Kutter</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Kutter :: início</title>
+        <link rel="stylesheet" href="style.css" />
+        <link rel="icon" type="image/x-icon" href="/assets/logo.svg" />
     </head>
+
     <body>
-        <h2>Início</h2>
+        <div class="container">
+            <div class="menubar">
+                <!--menu-->
+                <ul>
+                    <li class="menubar-item">
+                        <img class="icons-inverted" src="/assets/icons/home.svg" width="24px" height="24px"/> &nbsp;
+                        <a class="menubar-link" href="/" />Início</a>
+                    </li>
+                    <li class="menubar-item">
+                        <img class="icons-inverted" src="/assets/icons/circuit.svg" width="24px" height="24px"/> &nbsp;
+                        <a class="menubar-link" href="/circuitos" />Circuitos</a>
+                    </li>
+                    <li class="menubar-item">
+                        <img class="icons-inverted" src="/assets/icons/terminal.svg" width="24px" height="24px"/> &nbsp;
+                        <a class="menubar-link" href="/terminais" />Terminais</a>
+                    </li>
+                    <li class="menubar-item">
+                        <img class="icons-inverted" src="/assets/icons/plug.svg" width="24px" height="24px"/> &nbsp;
+                        <a class="menubar-link" href="/conectores" />Conectores</a>
+                    </li>
+                    <li class="menubar-item">
+                        <img class="icons-inverted" src="/assets/icons/splice.svg" width="24px" height="24px"/> &nbsp;
+                        <a class="menubar-link" href="/emendas" />Emendas</a>
+                    </li>
+                    <li class="menubar-item">
+                        <img class="icons-inverted" src="/assets/icons/cable.svg" width="24px" height="24px"/> &nbsp;
+                        <a class="menubar-link" href="/cabos" />Cabos</a>
+                    </li>
+                </ul>
+            </div>
+
+
+            <div>
+                <!--content-->
+                <div class="header-nav">
+                    <img src="/assets/logo.svg" alt="kutter's logo" width="36px" height="36px"/> &nbsp;
+                    <h2>Kutter<sub>v.0.0.1</sub></h2>
+                </div>
+                <br />
+                <section style="padding: 24px">
+                    <h1>Início</h1>
+                    <br />
+                    <p>⚠️​ Nenhum projeto ativo ainda :/</p>
+                    <br />
+                    <p>Gerencie o ciclo de vida de um projeto. Todos os seus projetos são exibidos nesta tela.</p>
+                    <br />
+                    <a href="" class="btns primary">Criar novo projeto</a>
+                </section>
+            </div>
+        </div>
     </body>
 </html>
